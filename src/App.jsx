4a74faca2168cd9cb7fbaa8d5006ac12658f76b3
Manish23cy027 <23cy027@skcet.ac.in>
2025-06-12 10:44:39 +0530

    removing path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
-// import AppAdmin from './Admin/AppAdmin';
 import WebsiteRouter from './WebsiteRouter';
 
 function App() {
   return (
     <Routes>
       <Route path="/*" element={<WebsiteRouter/>} />
-      {/* <Route path="/admin/*" element={<AppAdmin />} /> */}
     </Routes>
   );
 }
