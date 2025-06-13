f5a78e5dc0a157fc84dba609bde352badef6afd4
Manish23cy027 <23cy027@skcet.ac.in>
2025-06-13 11:58:29 +0530

    deployment

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react';
-import "../styles/Footer.css";
+import "../Styles/Footer.css";
 import { RiInstagramFill } from "react-icons/ri";
 import { ImLinkedin2 } from "react-icons/im";
 import { FaFacebookF } from "react-icons/fa6";
