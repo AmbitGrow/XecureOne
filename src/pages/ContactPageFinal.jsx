770ec3cd31c262c6b8ce570405346abb2d1c5ec8
Manish23cy027 <23cy027@skcet.ac.in>
2025-06-13 11:38:56 +0530

    Styles to styles

--- src/pages/ContactPageFinal.jsx
+++ src/pages/ContactPageFinal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect} from "react";
-import "../Styles/ContactpageFinal.css";
+import "../styles/ContactpageFinal.css";
 import { gsap } from "gsap";
 import axios from "axios";
 import { VscArrowCircleUp } from "react-icons/vsc";
